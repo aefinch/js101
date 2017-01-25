23f22a00ed5d55ab7ec9130f6a191a2b1645508d
Andrea Finch <aefinch@vt.edu>
2017-01-24 18:50:08 -0600

    removed console logs

--- sonnet.js
+++ sonnet.js
@@ -1,10 +1,9 @@
 var element = document.getElementById("sonnet");
 var sonnet = element.innerHTML;
-console.log(sonnet)
-console.log(sonnet.indexOf("orphans"));
-console.log(sonnet.length);
-sonnet = sonnet.replace("winter", "yuletide")
-console.log(sonnet);
+console.log("location of orphans", sonnet.indexOf("orphans"));
+console.log("char length of sonnet", sonnet.length);
+sonnet = sonnet.replace("winter", "yuletide");
+console.log("winter replaced", sonnet);
 sonnet = sonnet.replace(/the /g, "a large ");
-console.log(sonnet);
+console.log("final version", sonnet);
 element.innerHTML = sonnet;
