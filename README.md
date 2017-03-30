71cd4b4745508e63250c05a2fe220d94ce7f3557
Andrea Finch <aefinch@vt.edu>
2017-03-30 16:38:51 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-#JS101
+# JS101
+
+
+### Description
+These are my class notes from learning javascript.
+
+### Technologies Used
+- HTML
+- CSS
+- Javascript
+
+
+### How to Run
+1. git clone [here](https://github.com/aefinch/js101)
+1. cd to the file location
+1. Run http-server
+1. Navigate in browser to http://localhost:8080
+
+### Contributors
+[Andrea Finch](https://github.com/aefinch)
--- day3.js
+++ day3.js
@@ -16,3 +16,57 @@ for(var myKey in myCat) {
 	console.log("myKey ",myKey);
 	console.log("values", myCat[myKey]);
 }
+
+// Intro to Functions via Nuggets
+
+var nuggetElement = document.getElementById("nuggetShack");
+var availElement = document.getElementById("availForPurchase");
+console.log("nuggetElement", nuggetElement);
+console.log("availForPurchase", availForPurchase);
+
+function nuggetMaker(animal) {
+	var myNugget = "breading " + animal + " breading";
+	return myNugget;
+}
+
+function addBr(){
+	return "</br>";
+}
+function shackMaker(divLocation, typeOfNugget) {
+	var newBreak = addBr();
+	divLocation.innerHTML += typeOfNugget + newBreak;
+}
+
+
+
+var chickenNuggets = nuggetMaker("chicken");
+shackMaker(availElement, chickenNuggets);
+// nuggetElement.innerHTML = chickenNuggets;
+var lobsterNuggets = nuggetMaker("lobster");
+// nuggetElement.innerHTML = lobsterNuggets;
+shackMaker(availElement, lobsterNuggets);
+
+var tablets = nuggetMaker("myComputer");
+shackMaker(availElement, tablets);
+
+var shackName = "Callan's Shack";
+// nuggetElement.innerHTML+=shackName;
+shackMaker(nuggetElement, shackName);
+
+var animals = ["cats", "turtles", "fish", "bears", "tigers"];
+var colors = ["red", "blue", "gold", "pink", "orange"];
+function myLooper(myArray) {
+	for (var i = 0; i<myArray.length; i++) {
+		console.log(myArray[i]);
+	}
+}
+myLooper(animals);
+myLooper(colors);
+
+
+
+
+
+
+
+
